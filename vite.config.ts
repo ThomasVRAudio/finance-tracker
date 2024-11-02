0e5bd4c7c2293aef37ed41034246f115c2bba7a7
Thomas van Rossum <thomas_vr@hotmail.nl>
2024-11-02 22:30:43 +0100

    cleaned up errors

--- vite.config.ts
+++ vite.config.ts
@@ -10,4 +10,12 @@ export default defineConfig({
       overlay: false,
     },
   },
+  build: {
+    sourcemap: true, // Ensure source maps are enabled for the build
+  },
+  define: {
+    __VUE_PROD_DEVTOOLS__: false,
+    __VUE_OPTIONS_API__: true,
+    __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: false, // Set as needed
+  },
 });
